c9c62339eff847b9c7287e76a8958b996b9c0522
sevdeslbodur <bodursevdeisil@gmail.com>
2023-06-07 22:59:42 +0300

    test edildi,kontrol edildi

--- Issiz.py
+++ Issiz.py
@@ -1,47 +1,35 @@
 #Issiz.py
 
+
 from Insan import Insan
 
 class Issiz(Insan):
-    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, tecrubeler):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, statu):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
-        self.tecrubeler = tecrubeler
-        self.statu = self.statu_bul()
-        
-        
- #tecrübeye göre statü bulan yöntem 
- 
-    def __statu_bul(self):
-        try:
-            mavi_yaka = self.tecrubeler.get("mavi yaka", 0)
-            beyaz_yaka = self.tecrubeler.get("beyaz yaka", 0)
-            yonetici = self.tecrubeler.get("yönetici", 0)
-            
-            mavi_yaka_etkisi = mavi_yaka * 0.20
-            beyaz_yaka_etkisi = beyaz_yaka * 0.35
-            yonetici_etkisi = yonetici * 0.45
-            
-            max_etki = max(mavi_yaka_etkisi, beyaz_yaka_etkisi, yonetici_etkisi)
-            
-            if max_etki == mavi_yaka_etkisi:
-                return "mavi yaka"
-            elif max_etki == beyaz_yaka_etkisi:
-                return "beyaz yaka"
-            else:
-                return "yonetici"
-            
-        except Exception as e:
-            print("Hata:", str(e))
-            return None
+        self.statu = statu
+        self.tecrubeler = {}
 
     def __str__(self):
-        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nUygun Statü: {self.statu}"
+        return f"TC No: {self.tc_no}\nAd: {self.ad}\nSoyad: {self.soyad}\nYaş: {self.yas}\nCinsiyet: {self.cinsiyet}\nUyruk: {self.uyruk}\nStatü: {self.statu}"
 
-    def get_tecrubeler(self):
-        return self.tecrubeler
+    def tecrube_ekle(self, alan, tecrube):
+        self.tecrubeler[alan] = tecrube
 
-    def set_tecrubeler(self, tecrubeler):
-        self.tecrubeler = tecrubeler
+    def tecrube_sil(self, alan):
+        if alan in self.tecrubeler:
+            del self.tecrubeler[alan]
 
-    def get_statu(self):
-        return self.statu
+    def tecrube_getir(self, alan):
+        if alan in self.tecrubeler:
+            return self.tecrubeler[alan]
+        else:
+            return None
+
+    def ortalama_tecrube(self):
+        if len(self.tecrubeler) > 0:
+            toplam_tecrube = sum(self.tecrubeler.values())
+            ortalama = toplam_tecrube / len(self.tecrubeler)
+            return ortalama
+        else:
+            return 0
+
